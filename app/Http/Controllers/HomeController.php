be870fef0d4c1a0507c6efa19965acc77a9424e6
Fulliton <artyshko.andrey@gmail.com>
2020-07-08 23:01:21 +0700

    Редизайн

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,19 +10,23 @@ class HomeController extends Controller
     function pay() {
         $request = [
             'pg_merchant_id' => '530044',
-            'pg_testing_mode' => false,
+            'pg_testing_mode' => 0,
             'pg_currency' => 'RUB', // Валюта
-            'pg_amount' => '1500', // Стоимость
+            'pg_amount' => '2500', // Стоимость
+            'pg_order_id'=>'123',
             'pg_salt' => 'randomStringForProfessionModel',
             'pg_success_url_method' => 'GET',
             'pg_description' => 'Приобретение "Умный ёжик"',
             'pg_success_url' => route('pay.success'),
-            'pg_result_url' => route('index')
+            'pg_result_url' => route('index'),
+            
         ];
         ksort($request); //sort alphabetically
         array_unshift($request, 'payment.php');
         array_push($request, 'JLx0OEMuSkWeALYH');
+        
         $request['pg_sig'] = md5(implode(';', $request));
+        
         unset($request[0], $request[1]);
         $query = http_build_query($request);
         return redirect('https://api.paybox.money/payment.php?' . $query);
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description" content="Сайт">
     <title>Умный ёжик</title>
-
     <!--[if lt IE 9]>
     <script src="https://www.html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <!-- FONTS -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <!-- CSS -->
@@ -45,7 +44,7 @@
 
 <main class="main" id="main">
     <header class="header">
-        <div class="container">
+        <div class="container-custom">
             <a href="{{ route('index') }}" class="header-logo">
                 <img src="{{ asset('img/main/main-logo.png') }}" alt="">
             </a>
@@ -71,7 +70,7 @@
                 <span></span>
             </button>
         </div>
-        <!-- /.container -->
+        <!-- /.container-custom -->
     </header>
     <!-- /.header -->
     @if(Session::has('message'))
@@ -79,11 +78,11 @@
             {{Session::get('message')}}
         </div>
     @endif
-    <div class="container">
-        <div class="main-blockTitle" id="main-blockTitle">
+    <div class="container-custom">
+        <div class="main-blockTitle pt-1" id="main-blockTitle">
             <h1 class="main-title">Онлайн детский сад</h1>
         </div>
-        <p class="main-subtitle">по авторской методике Юлии Чучуло</p>
+        <p class="main-subtitle">Курс раннего развития ребёнка по авторской методике Юлии Чучуло</p>
         <h2 class="main-tagline">Мы успешно развиваем</h2>
         <div class="main-list">
             <div class="main-item">
@@ -108,14 +107,14 @@
             <!-- /.main-item -->
         </div>
         <!-- /.main-list -->
-        <button class="main-btn section-btn">Получите доступ на месяц <span></span></button>
+        <a href="#offer" class="btn orange-link-btn col-sm-4 py-4 mt-3 w-100 px-3 ">Получите доступ на месяц всего за 2 500 р.<span></span></a>
     </div>
-    <!-- /.container -->
+    <!-- /.container-custom -->
 </main>
 <!-- /.main -->
 
-<section class="question" id="question">
-    <div class="container">
+<section class="question pb-5 mb-5" id="question">
+    <div class="container-custom">
         <div class="question-block question-block_1">
             <h3 class="question-block__title">Хотите дать своему малышу лучшее,</h3>
             <p class="question-block__text">но не знаете, что для этого сделать?</p>
@@ -135,16 +134,212 @@
         </div>
         <!-- /.question-block question-block_3 -->
     </div>
-    <!-- /.container -->
+    <!-- /.container-custom -->
 </section>
 <!-- /.question -->
 
+<section id="complecs" class="py-5 mb-5">
+  <div class="container-fluid px-md-5">
+    <div class="row justify-content-center">
+      <div class="col-md-6 text-center col-12">
+        <h2 class="font-weight-bold section-title text-green-gradient">В онлайн детском садике «Умный Ёжик» вас ждёт комплексная программа развития и малыша, и мамы</h2>
+      </div>
+    </div>
+
+    <div class="row mt-5 align-items-center justify-content-center">
+      <div class="col-md-5"><img class="img-fluid w-100" src="{{ asset('img/complecs/undraw_time_management_30iu 1.svg') }}" alt="undraw_time_management_30iu 1.svg"></div>
+      <div class="col-md-6 col-10 mt-3 mt-md-0">
+        <div class="row">
+          <div class="col-md-6 offset-md-2 text-center text-md-left">
+            <p class="h3 font-weight-normal">Вам скучно проводить целый день со своим малышом?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mt-5 align-items-center justify-content-center">
+      <div class="col-md-5 order-md-12"><img class="img-fluid w-100" src="{{ asset('img/complecs/undraw_feeling_blue_4b7q 1.svg') }}" alt="undraw_time_management_30iu 1.svg"></div>
+      <div class="col-md-6 col-10 order-md-1 mt-3 mt-md-0">
+        <div class="row">
+          <div class="col-md-6 offset-md-2 text-center text-md-left">
+            <p class="h3 font-weight-normal">Быстро устаете от совместных игр и не знаете, что придумать еще?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mt-5 align-items-center justify-content-center">
+      <div class="col-md-5"><img class="img-fluid w-100" src="{{ asset('img/complecs/undraw_quality_time_wiyl 1.svg') }}" alt="undraw_time_management_30iu 1.svg"></div>
+      <div class="col-md-6 col-10 mt-3 mt-md-0">
+        <div class="row">
+          <div class="col-md-6 offset-md-2 text-center text-md-left">
+            <p class="h3 font-weight-normal">Не вовлекаетесь в деятельность ребенка и не знаете, как себя заставить?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row mt-5 align-items-center justify-content-center">
+      <div class="col-md-6 order-md-12"><img class="img-fluid w-100" src="{{ asset('img/complecs/undraw_searching_p5ux 1.svg') }}" alt="undraw_time_management_30iu 1.svg"></div>
+      <div class="col-md-5 col-10 order-md-1 mt-3 mt-md-0">
+        <div class="row">
+          <div class="col-md-6 offset-md-2 text-center text-md-left">
+            <p class="h3 font-weight-normal">Не знаете, с чего начать и когда лучше приступить к занятиям с ребенком?</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row justify-content-center mt-5 pt-5">
+      <div class="col-10">
+        <div class="card border-0 bg-white" id="img-like">
+          <div class="card-body border-0 pt-5 pb-5 mt-4">
+            <div class="row">
+              <div class="col-md-4 offset-md-1">
+                <div class="row">
+                  <div class="col-12">
+                    <p class="h4 font-weight-bold">Впервые все секретные техники педагогов раннего развития доступны каждой маме!</p>
+                  </div>
+                  <div class="col-12">
+                    <a href="#offer" class="btn orange-link-btn py-4 mt-3 w-100 px-3 ">Получите доступ на месяц всего за 2 500 р.<span></span></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row justify-content-center mt-5 pt-5">
+      <div class="col-10 mt-md-5">
+        <div class="card border-0 bg-white mt-md-5" style="border-radius: 41px;">
+          <div class="card-body">
+            <div class="row p-4 mt-3">
+              <div class="col-lg-6 offset-lg-1">
+                <p class="h3 font-weight-bold">
+                  Больше не нужно искать детские центры или сады с качественными педагогами и эффективными методиками
+                </p>
+                <p class="font-weight-normal h5 pt-3">С нашей авторской программой педагогической подготовки мамы вы станете первым и лучшим учителем для своего малыша.</p>
+              </div>
+              <img src="{{ asset('img/complecs/Group 87.svg') }}" alt="group 87" class="position-absolute d-none d-lg-block" id="img-girl">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section>
+
+<section id="complecs" class="py-5 mb-5">
+  <div class="container-fluid px-md-5">
+    <div class="row justify-content-center">
+      <div class="col-md-6 text-center col-12">
+        <h2 class="font-weight-bold section-title text-green-gradient"> «Мама – учитель №1»</h2>
+      </div>
+    </div>
+
+    <div class="row justify-content-center align-self-stretch mt-5 px-md-5">
+      <div class="col-lg-3 col-md-4 col-sm-6 my-3 my-md-0">
+        <div class="card h-100 border-0 card-mom">
+          <div class="card-body p-0">
+            <div class="row">
+              <div class="col-12">
+                <img src="{{ asset('img/complecs/photo-1.png') }}" alt="photo-1" class="img-fluid w-100">
+              </div>
+              <div class="col-12 px-4 m-2 mt-3 mb-3">
+                <p class="font-weight-bold h4" style="color: #28A352;">Живые вебинары</p>
+                <p class="h5">по эффективным техникам развития ребенка дома.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-4 col-sm-6 my-3 my-md-0">
+        <div class="card h-100 border-0 card-mom">
+          <div class="card-body p-0">
+            <div class="row">
+              <div class="col-12">
+                <img src="{{ asset('img/complecs/photo-2.png') }}" alt="photo-2" class="img-fluid w-100">
+              </div>
+              <div class="col-12 px-4 m-2 mt-3 mb-3">
+                <p class="font-weight-bold h4" style="color: #28A352;">Интересные видеоуроки</p>
+                <p class="h5">с заданиями на прокачку новых навыков.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-4 col-sm-6 my-3 my-md-0">
+        <div class="card h-100 border-0 card-mom">
+          <div class="card-body p-0">
+            <div class="row">
+              <div class="col-12">
+                <img src="{{ asset('img/complecs/photo-3.png') }}" alt="photo-3" class="img-fluid w-100">
+              </div>
+              <div class="col-12 px-4 m-2 mt-3 mb-3">
+                <p class="font-weight-bold h4" style="color: #28A352;">Закрытый чат</p>
+                <p class="h5">поддержки с куратором и автором программы.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-4 col-sm-6 my-3 my-md-0">
+        <div class="card h-100 border-0 card-mom">
+          <div class="card-body p-0">
+            <div class="row">
+              <div class="col-12">
+                <img src="{{ asset('img/complecs/photo-4.png') }}" alt="photo-1" class="img-fluid w-100">
+              </div>
+              <div class="col-12 px-4 m-2 mt-3 mb-3">
+                <p class="font-weight-bold h4" style="color: #28A352;">Полезные материалы</p>
+                <p class="h5">дополнительные материалы к каждому уроку.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row justify-content-center mt-5">
+      <div class="col-10">
+        <div class="card border-0 bg-white card-calendar">
+          <div class="card-body border-0 pt-5 pb-5 mt-4">
+            <div class="row px-1">
+{{--              <div class="col-md-2 col-8 pb-4 align-items-center d-flex d-md-none mr-auto ml-auto">--}}
+{{--                <img src="{{ asset('img/complecs/Group 96.svg') }}" alt="" class="img-fluid">--}}
+{{--              </div>--}}
+              <div class="col-md-4 offset-md-1">
+                <div class="row">
+                  <div class="col-12">
+                    <p class="h5 font-weight-normal" style="font-family: 'Circe'">А также полный доступ на месяц занятий для малыша по авторской методике "Умный Ёжик! Занятия по грамматике, математике и, конечно, увлекательные творческие поделки и опыты.</p>
+                  </div>
+                  <div class="col-12">
+                    <a href="#offer" class="btn orange-link-btn py-4 mt-3 w-100 px-3 ">Получите доступ на месяц всего за 2 500 р.<span></span></a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-2 align-items-center mr-auto d-none d-md-flex ml-auto">
+                <img src="{{ asset('img/complecs/Group 96.svg') }}" alt="" class="img-fluid">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</section>
+
+
 <section class="plan" id="plan">
-    <h2 class="plan-title section-title">Программа развития нужных психологических функций и моторных навыков для деток 2+</h2>
-    <div class="container">
-        <div class="plan-item plan-item_1"><img src="{{ asset('img/plan/img-1.png') }}" alt="" class="plan-item__img"><span><strong>36 занятий в месяц.</strong></span><img src="{{ asset('img/plan/line.svg') }}" alt="" class="plan-line"></div>
+    <h2 class="plan-title section-title text-center ml-auto mr-auto">Программа развития нужных психологических функций и моторных навыков для деток 2+</h2>
+    <div class="container-custom">
+        <div class="plan-item plan-item_1"><img src="{{ asset('img/plan/img-1.png') }}" alt="" class="plan-item__img"><span>36 видеоуроков для самостоятельных занятий с малышом дома</span><img src="{{ asset('img/plan/line.svg') }}" alt="" class="plan-line"></div>
         <!-- /.plan-item plan-item_1 -->
-        <div class="plan-item plan-item_2"><img src="{{ asset('img/plan/img-2.png') }}" alt="" class="plan-item__img"><span><strong>Занятия от 15 минут в день.</strong></span></div>
+        <div class="plan-item plan-item_2"><img src="{{ asset('img/plan/img-2.png') }}" alt="" class="plan-item__img"><span>занятие длится от 15 минут в день</span></div>
         <!-- /.plan-item plan-item_2 -->
         <div class="plan-item plan-item_3"><img src="{{ asset('img/plan/img-3.png') }}" alt="" class="plan-item__img"><span><strong>Со всеми необходимыми</strong> авторскими пособиями для печати.</span></div>
         <!-- /.plan-item plan-item_3 -->
@@ -152,8 +347,8 @@
         <!-- /.plan-item plan-item_4 -->
         <img src="{{ asset('img/plan/mobile-img.png') }}" alt="" class="plan-img">
     </div>
-    <!-- /.container -->
-    <button class="plan-btn section-btn">Получите доступ на месяц <span></span></button>
+    <!-- /.container-custom -->
+    <a href="#offer" class="btn orange-link-btn py-4 ml-auto mr-auto d-block col-sm-3 mt-3 w-100 px-3 ">Получите доступ на месяц всего за 2 500 р.<span></span></a>
 </section>
 <!-- /.plan -->
 
@@ -202,7 +397,7 @@
 
 <section class="result" id="result">
     <h2 class="result-title section-title">Результаты обучения по методике</h2>
-    <div class="container">
+    <div class="container-custom">
         <div class="result-block">
             <img src="{{ asset('img/result/img-1.png') }}" alt="" class="result-block__img">
             <img src="{{ asset('img/result/mobile-img-1.png') }}" alt="" class="result-block__mobileImg">
@@ -231,13 +426,13 @@
             <img src="{{ asset('img/result/img-2.png') }}" alt="" class="result-block__img">
         </div>
     </div>
-    <!-- /.container -->
+    <!-- /.container-custom -->
 </section>
 <!-- /.result -->
 
 <section class="comment" id="comment">
     <h2 class="comment-title section-title">Отзывы о нашей работе</h2>
-    <div class="container">
+    <div class="container-custom">
         <div class="comment-slider">
 
             <div class="comment-slide">
@@ -285,19 +480,19 @@
             </div>
         </div>
     </div>
-    <!-- /.container -->
+    <!-- /.container-custom -->
 </section>
 <!-- /.comment -->
 
 <section class="offer" id="offer">
     <img src="{{ asset('img/offer/offer-bg.png') }}" alt="" class="offer-bg">
-    <div class="container">
+    <div class="container-custom">
         <div class="offer-access">
             <h2 class="offer-access__title">Ограниченное предложение</h2>
             <svg width="172" height="296" viewBox="0 0 172 296" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M103.523 291.232C103.667 292.048 104.445 292.592 105.261 292.448L118.555 290.096C119.371 289.952 119.915 289.174 119.771 288.358C119.626 287.542 118.848 286.998 118.032 287.142L106.216 289.232L104.126 277.416C103.981 276.6 103.203 276.056 102.387 276.2C101.571 276.344 101.027 277.122 101.171 277.938L103.523 291.232ZM1.592 28.4141C58.5617 44.5134 99.108 88.4926 117.906 139.24C136.709 190.004 133.682 247.343 103.771 290.111L106.229 291.83C136.817 248.095 139.79 189.684 120.719 138.198C101.642 86.6956 60.4381 41.9262 2.40783 25.5271L1.592 28.4141Z" fill="#B2DB34"/>
             </svg>
-            <p class="offer-access__text">Успейте приобрести программу “Умный Ёжик” по самой выгодной цене</p>
+            <p class="offer-access__text">Успейте приобрести программу “Умный Ёжик” по самой выгодной цене до конца июля</p>
             <button class="offer-access__btn section-btn" id="paybox">
                 получите доступ на месяц
                 <img src="{{ asset('img/offer/discount.png') }}" alt="">
@@ -307,10 +502,13 @@
         <div class="offer-features">
             <h3 class="offer-features__title">Приобретая абонемент на месяц, вы получаете:</h3>
             <div class="offer-item">
+              <img src="{{ asset('img/offer/uil_clock.png') }}" alt=""><span> Доступ к обучающей программе для мамы: <br> <span style="color: #28A352;">«Мама-учитель №1»</span></span>
+            </div>
+            <div class="offer-item">
                 <img src="{{ asset('img/offer/icon-time.svg') }}" alt="">36 занятий по 15 минут
             </div>
             <div class="offer-item">
-                <img src="{{ asset('img/offer/icon-lesson.svg') }}" alt="">12 сопроводительных занятий для мамы
+                <img src="{{ asset('img/offer/icon-lesson.svg') }}" alt="">занятие длится от 15 минут в день
             </div>
             <div class="offer-item">
                 <img src="{{ asset('img/offer/icon-creation.svg') }}" alt="">Увлекательные творческие задания и опыты
@@ -319,16 +517,16 @@
                 <img src="{{ asset('img/offer/icon-support.svg') }}" alt="">Онлайн поддержка от самого автора программы
             </div>
             <div class="offer-item">
-                <img src="{{ asset('img/offer/icon-global.svg') }}" alt="">Живые эфиры, со встречей новых друзей и полезной информацией
+                <img src="{{ asset('img/offer/icon-global.svg') }}" alt="">12 мини-лекций с лайфхаками маме для эффективных занятий!
             </div>
         </div>
     </div>
-    <!-- /.container -->
+    <!-- /.container-custom -->
 </section>
 <!-- /.offer -->
 
 <div class="gallery" id="gallery">
-    <div class="container">
+    <div class="container-custom">
         <h2 class="gallery-title section-title">Наша жизнь</h2>
         <div class="gallery-slider">
             <img src="{{ asset('img/gallery/img-1.jpg') }}" alt="" class="gallery-slider__img">
@@ -371,7 +569,7 @@
             </div>
         </div>
     </div>
-    <!-- /.container -->
+    <!-- /.container-custom -->
 </div>
 <!-- /.gallery -->
 
